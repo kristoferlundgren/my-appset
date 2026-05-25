e72cea73458dd4407a737fdf4f452c8c22f91934
Kristofer Lundgren <kristofer@famlundgren.org>
2026-05-25 23:40:03 +0200

    Fix typo in path setting in bootstrap.sh

--- AppSet/bootstrap.sh
+++ AppSet/bootstrap.sh
@@ -4,7 +4,7 @@ helm template . \
     --set applicationProject=<project> \
     --set name=<name> \
     --set namespace=<namespace> \
-    --set path=<<path> \
+    --set path=<path> \
     --set repoURL=<repoURL> \
     --set revision=main \
   | kubectl apply -f -
